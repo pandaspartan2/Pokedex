57f54225c851b37421be27a9f0638cdff97fd490
pandaspartan2 <90705933+pandaspartan2@users.noreply.github.com>
2021-10-19 09:13:55 -0500

    pokemon layout done

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -15,18 +15,31 @@ struct ContentView: View {
                     .font(.title2)
                     .fontWeight(.bold)
                     .foregroundColor(.white)
+                    .padding(.top, 10)
+                    .padding(.leading)
                 
                 HStack {
                     Text("Poison")
                         .font(.caption)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 8)
+                        .overlay(RoundedRectangle (cornerRadius: 12)
+                                    .fill(Color.white.opacity(0.25))
+                        )
+                        
+                        .frame(width: 100, height: 25)
                     
                     Text("Grass")
                         .font(.caption)
                         .fontWeight(.bold)
                         .foregroundColor(.white)
-                    
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 8)
+                        .overlay(RoundedRectangle (cornerRadius: 12)
+                        .fill(Color.white.opacity(0.25))
+                    )
                     Image("1")
                         .resizable()
                         .scaledToFit()
@@ -37,6 +50,7 @@ struct ContentView: View {
         }
         .background(Color.green)
         .cornerRadius(12)
+        .shadow(color: .green, radius: 8, x: 0.0, y: 0.0)
     }
 }
 
