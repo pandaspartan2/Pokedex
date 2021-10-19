f5900b19a1ed37fa95f047ffda0f9ee9208cee04
pandaspartan2 <90705933+pandaspartan2@users.noreply.github.com>
2021-10-19 08:30:17 -0500

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by AMStudent on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
