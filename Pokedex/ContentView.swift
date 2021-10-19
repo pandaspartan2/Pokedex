03b7f8f706d4fd275207240c4784323b6826be4b
pandaspartan2 <90705933+pandaspartan2@users.noreply.github.com>
2021-10-19 09:00:11 -0500

    added bulbasaur

--- Pokedex/ContentView.swift
+++ Pokedex/ContentView.swift
@@ -9,8 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Bulbasaur")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                HStack {
+                    Text("Poison")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Text("Grass")
+                        .font(.caption)
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                    
+                    Image("1")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 68, height: 68)
+                        .padding([.bottom, .trailing], 5)
+                }
+            }
+        }
+        .background(Color.green)
+        .cornerRadius(12)
     }
 }
 
